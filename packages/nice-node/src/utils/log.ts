23b703007737b7eb736a60936caa623ab16c2f60
zhi.zhong <zhong.zhi@163.com>
2020-03-01 15:39:18 +0800

    chore: 重写 npmlog 默认样式

--- packages/nice-node/src/utils/log.ts
+++ packages/nice-node/src/utils/log.ts
@@ -2,7 +2,17 @@ import npmlog, { log, LogLevels } from 'npmlog';
 
 export default (level: LogLevels | string, message: string, ...args: any[]) => {
   const { PROFILE, NODE_ENV } = process.env;
-  const prefix: string = 'nice-node';
+
+  // 重写 npmlog 默认样式
+  const prefix: string = '[nice-node]';
+  npmlog.addLevel('silly', -Infinity, { inverse: true }, `${prefix} sill`);
+  npmlog.addLevel('verbose', 1000, { fg: 'blue', bg: 'black' }, `${prefix} verb`);
+  npmlog.addLevel('info', 2000, { fg: 'green' }, prefix);
+  npmlog.addLevel('timing', 2500, { fg: 'green', bg: 'black' }, prefix);
+  npmlog.addLevel('http', 3000, { fg: 'green', bg: 'black' }, prefix);
+  npmlog.addLevel('notice', 3500, { fg: 'blue', bg: 'black' }, prefix);
+  npmlog.addLevel('warn', 4000, { fg: 'black', bg: 'yellow' }, `${prefix} WARN`);
+  npmlog.addLevel('error', 5000, { fg: 'red', bg: 'black' }, `${prefix} ERR!`);
 
   /* istanbul ignore if */
   if (PROFILE !== 'local') {
@@ -14,5 +24,5 @@ export default (level: LogLevels | string, message: string, ...args: any[]) => {
     npmlog.level = 'silent';
   }
 
-  log(level, prefix, message, ...args);
+  log(level, '', message, ...args);
 };
--- packages/nice-node/src/utils/version.ts
+++ packages/nice-node/src/utils/version.ts
@@ -6,4 +6,4 @@ const pkgPath = join(__dirname, '../../package.json');
 const pkgContent = readFileSync(pkgPath, 'utf-8');
 const { version: nodeVersion } = JSON.parse(pkgContent);
 
-log('info', `nice-node: ${nodeVersion}\nnode: ${process.versions.node}\nuser-agent: ${process.env.npm_config_user_agent}`);
+log('info', `\nnice-node: ${nodeVersion}\nnode: ${process.versions.node}\nuser-agent: ${process.env.npm_config_user_agent}`);
