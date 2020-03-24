ccf30bce96defc586378ed3fbb90b77444008c07
zhi.zhong <zhong.zhi@163.com>
2020-03-24 16:58:08 +0800

    fix: Cannot find module 'knex'

--- src/utils/knex.ts
+++ src/utils/knex.ts
@@ -1,4 +1,4 @@
-const { KNEX_CLIENT } = process.env;
+const { KNEX_CLIENT, MYSQL_ENABLE } = process.env;
 
 // eslint-disable-next-line global-require
-export default require('knex')({ client: KNEX_CLIENT });
+export default MYSQL_ENABLE === 'true' ? require('knex')({ client: KNEX_CLIENT }) : null;
