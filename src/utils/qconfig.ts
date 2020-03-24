b8e82c76b37ece6e2c692d789e072b500c4a465a
zhi.zhong <zhong.zhi@163.com>
2020-03-25 07:10:31 +0800

    feat: 支持q配置

---
+++ src/utils/qconfig.ts
@@ -0,0 +1,74 @@
+import { resolve } from 'path';
+import { existsSync } from 'fs';
+import qconfigClient from '@qnpm/qconfig-client-plus';
+import deepmerge from 'deepmerge';
+
+export interface QconfigOptions {
+  enable?: boolean;
+  options?: {
+    name: string;
+    token: string;
+    properties?: string[];
+    timerInterval?: number;
+    logPath?: string;
+    useCluster?: boolean;
+  }
+}
+
+/**
+ * 在灰度或者prod环境下检查拉取到的qconnfig是否是线上环境
+ */
+// const prodReg = /prod|prepare/;
+// function checkQconfigEnv(): void {
+//   // 这里需要检查环境进行，环境的拉取判断
+//   if (prodReg.test(NODE_ENV)) {
+//     const qconfigEnv = profileConfig.rawValue('env');
+
+//     if (qconfigEnv !== 'prod') {
+//       throw new Error(`qconfig环境检查失败，拉取到的配置文件环境是: ${qconfigEnv}`);
+//     }
+//   }
+// }
+
+/**
+ * 执行初始化config
+ */
+export default async (opts: QconfigOptions = {}) => {
+  const {
+    QCONFIG_ENABLE,
+    QCONFIG_TOKEN,
+    QCONFIG_PROPERTIES,
+    QCONFIG_TIMERINTERVAL,
+    QCONFIG_USECLUSTER,
+    APP_CODE,
+    LOG_ROOT = ''
+  } = process.env;
+
+  // qconfigClient初始化参数
+  const defaultOptions = {
+    enable: QCONFIG_ENABLE === 'true',
+    options: {
+      name: APP_CODE,
+      token: QCONFIG_TOKEN,
+      properties: QCONFIG_PROPERTIES?.split(','),
+      timerInterval: QCONFIG_TIMERINTERVAL,
+      logPath: resolve(LOG_ROOT),
+      useCluster: QCONFIG_USECLUSTER === 'true'
+    }
+  };
+
+  const {
+    enable,
+    options
+  } = deepmerge(defaultOptions, opts);
+
+  if (enable) {
+    const files = ['pom.xml', 'package.json'];
+    if (files.every((file) => existsSync(resolve(file)))) {
+      await qconfigClient.init(options);
+    } else {
+      console.error(`qconfig client can not found ${files.join(',')} at project root.`);
+    }
+  }
+  // checkQconfigEnv();
+};
--- test/utils/qconfig.ts
+++ test/utils/qconfig.ts
@@ -1,8 +1,18 @@
 import should from 'should';
+import client from '@qnpm/qconfig-client-plus';
 import qconfig from '../../src/utils/qconfig';
 
 describe('utils/qconfig.ts', () => {
-  it('should create log file', async () => {
-    await qconfig();
+  it.skip('should get config data', async () => {
+    await qconfig({
+      enable: true,
+      options: {
+        name: '',
+        token: '',
+        properties: ['tipsText.json'],
+        logPath: 'logs/qconfig'
+      }
+    });
+    client.getConfig('tipsText.json').get('NoticeTips').noticeTitle.should.equal('公告栏');
   });
 });
